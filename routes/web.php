b9fd21f6c2b4cb3798185240802b1592eb623e67
Daniel Jackson <djackson@uabc.edu.mx>
2018-11-06 03:37:06 -0800

    Mas:(

--- routes/web.php
+++ routes/web.php
@@ -24,31 +24,31 @@ Route::get('/animales',function(){
     $octp = DB::table('octopuses')->get();
     
 return $ante . $du . $mares . $vicu . $fro . $octp;
-});
+})->middleware('grupo1');;
 
 
 Route::get('animales/{$id}', function($id){
-  $an=App\antelopes::find($id)->get();
-  $du=App\dugongs::find($id)->get();
-  $ma=App\mares::find($id)->get();
-  $vi=App\vicunas::find($id)->get();
-  $fr=App\frogs::find($id)->get();
-  $oc=App\octopuses::find($id)->get();
+  $an=App\antelopes::find($id)->get($id);
+  $du=App\dugongs::find($id)->get($id);
+  $ma=App\mares::find($id)->get($id);
+  $vi=App\vicunas::find($id)->get($id);
+  $fr=App\frogs::find($id)->get($id);
+  $oc=App\octopuses::find($id)->get($id);
   return $an . $du . $ma . $vi . $fr . $oc ;
-});
+})->middleware('grupo1');;
 
-Route::post('Frog/add',function(Request $request){
-    $frog = new Frog;
-    $frog -> nombre = $request->input('nombre');
-    $frog -> color = $request->input('color');
-    $frog -> peso = $request->input('peso');
-    $frog -> longitud = $request->input('longitud');
-    $frog -> edad = $request->input('edad');
-    $frog -> animals_id = $request->input('animals_id');
-    $frog ->save();
+// Route::post('Frog/add',function(Request $request){
+//     $frog = new Frog;
+//     $frog -> nombre = $request->input('nombre');
+//     $frog -> color = $request->input('color');
+//     $frog -> peso = $request->input('peso');
+//     $frog -> longitud = $request->input('longitud');
+//     $frog -> edad = $request->input('edad');
+//     $frog -> animals_id = $request->input('animals_id');
+//     $frog ->save();
 
-    echo 'La id' . $frog;
-});
+//     echo 'La id' . $frog;
+// });
 
 
 
