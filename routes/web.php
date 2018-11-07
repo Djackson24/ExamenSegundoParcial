1116349e70e4de166812e71efa9e18b0eb85e4ec
Daniel Jackson <djackson@uabc.edu.mx>
2018-11-06 21:26:57 -0800

    masmas

--- routes/web.php
+++ routes/web.php
@@ -94,7 +94,7 @@ Route::get('/octopuses/delete/{id}', function ($id) {
 Route::get('Mares/{id}',function($id){
     $us = Mare::find($id);
     return $us;
-})->where(['id' => '[\d]+']);
+})->where(['id' => '[\d]+'])->middleware('grupo1');
 
 Route::get('Antelope/{id}',function($id){
     $us = Antelope::find($id);
