96df8c7e1801df9950404a2717a1cd493c147eff
Daniel Jackson <djackson@uabc.edu.mx>
2018-11-06 18:29:11 -0800

    MasMasMas

--- routes/web.php
+++ routes/web.php
@@ -89,8 +89,60 @@ Route::post('mares/add', function(Request $request){
     $mares -> save();
 })->middleware('grupo2');
 
+Route::post('dugongs/add', function(Request $request){
+    $dugongs = new Dugong;
+    $dugongs -> nombre = $request->input('nombre');
+    $dugongs -> color = $request->input('color');
+    $dugongs -> peso= $request->input('peso');
+    $dugongs -> longitud = $request->input('longitud');
+    $dugongs -> edad = $request->input('edad');
+    $dugongs -> animal_id = $request->input('animal_id');
+    $dugongs -> save();
+})->middleware('grupo2');
 
+Route::post('vicunas/add', function(Request $request){
+    $vicunas = new Vicuna;
+    $vicunas-> nombre = $request->input('nombre');
+    $vicunas-> color = $request->input('color');
+    $vicunas-> peso= $request->input('peso');
+    $vicunas-> longitud = $request->input('longitud');
+    $vicunas-> edad = $request->input('edad');
+    $vicunas-> animal_id = $request->input('animal_id');
+    $vicunas-> save();
+})->middleware('grupo2');
 
+Route::post('antelopes/add', function(Request $request){
+    $antelopes = new Antelope;
+    $antelopes-> nombre = $request->input('nombre');
+    $antelopes-> color = $request->input('color');
+    $antelopes-> peso= $request->input('peso');
+    $antelopes-> longitud = $request->input('longitud');
+    $antelopes-> edad = $request->input('edad');
+    $antelopes-> animal_id = $request->input('animal_id');
+    $antelopes-> save();
+})->middleware('grupo2');
+
+Route::post('frogs/add', function(Request $request){
+    $frogs= new Frog;
+    $frogs-> nombre = $request->input('nombre');
+    $frogs-> color = $request->input('color');
+    $frogs-> peso= $request->input('peso');
+    $frogs-> longitud = $request->input('longitud');
+    $frogs-> edad = $request->input('edad');
+    $frogs-> animal_id = $request->input('animal_id');
+    $frogs-> save();
+})->middleware('grupo2');
+
+Route::post('octopuses/add', function(Request $request){
+    $octopuses= new Octopus;
+    $octopuses-> nombre = $request->input('nombre');
+    $octopuses-> color = $request->input('color');
+    $octopuses-> peso= $request->input('peso');
+    $octopuses-> longitud = $request->input('longitud');
+    $octopuses-> edad = $request->input('edad');
+    $octopuses-> animal_id = $request->input('animal_id');
+    $octopuses-> save();
+})->middleware('grupo2');
 
 
 
